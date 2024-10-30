6c82cdd42ed07a29e2a83e78f5bb4070dcfe9401
PizzaPapst <maik.bartels2@gmail.com>
2024-10-30 15:53:53 +0100

    add a simple test

---
+++ backend/src/test/java/com/cityfeedback/backend/simpleTest.java
@@ -0,0 +1,13 @@
+package com.cityfeedback.backend;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class simpleTest {
+
+    @Test
+    void additionTest() {
+        int result = 1 + 1;
+        assertEquals(2, result, "1 + 1 sollte 2 ergeben");
+    }
+}
