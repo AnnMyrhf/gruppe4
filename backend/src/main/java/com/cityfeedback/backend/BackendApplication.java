645b2f0741be9ade4c05ad05bd8835f8b6c5f777
Ann-Kathrin <ann-kathrin.meyerhof@stud.th-luebeck.de>
2024-11-03 17:05:42 +0100

    Dependencies in pom.xml ueberarbeitet, SysOut in main-Klasse geloescht

--- backend/src/main/java/com/cityfeedback/backend/BackendApplication.java
+++ backend/src/main/java/com/cityfeedback/backend/BackendApplication.java
@@ -3,6 +3,9 @@ package com.cityfeedback.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Hauptklasse, welche die Anwendung ausfuehrt
+ */
 @SpringBootApplication
 public class BackendApplication {
 
