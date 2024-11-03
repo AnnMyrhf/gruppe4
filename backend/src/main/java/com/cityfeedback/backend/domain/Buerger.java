d71ec246a5c07476cbeaddf0b1cb44b5ef7f6741
Ann-Kathrin <ann-kathrin.meyerhof@stud.th-luebeck.de>
2024-11-03 18:39:04 +0100

    Entitaet "Buerger" erstellt

--- backend/src/main/java/com/cityfeedback/backend/domain/Buerger.java
+++ backend/src/main/java/com/cityfeedback/backend/domain/Buerger.java
@@ -1,4 +1,30 @@
 package com.cityfeedback.backend.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ * Repraesentiert einen Buerger in der CityFeedback-Anwendung
+ */
+@Entity // JPA-Entitaet fuer DB
+@Data // automatisch Getter, Setter, toString() usw
+@NoArgsConstructor // leerer Konstruktor fuer JPA benoetigt
+@AllArgsConstructor
 public class Buerger {
+
+    @Id // Markiert id als Primaerschluessel
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // id wird automatisch inkrementiert
+    private String id;
+    private String anrede;
+    private String vorname;
+    private String nachname;
+    private String telefonnummer;
+    private String email;
+    private String passwort;
 }
