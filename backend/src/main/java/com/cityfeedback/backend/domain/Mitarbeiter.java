b987622fda7ddd68c3769faffdc0ef600081fb5a
PizzaPapst <maik.bartels2@gmail.com>
2024-11-04 11:21:59 +0100

    init mitarbeiter

--- backend/src/main/java/com/cityfeedback/backend/domain/Mitarbeiter.java
+++ backend/src/main/java/com/cityfeedback/backend/domain/Mitarbeiter.java
@@ -1,4 +1,33 @@
 package com.cityfeedback.backend.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ * Repraesentiert einen Buerger in der CityFeedback-Anwendung
+ */
+@Entity // JPA-Entitaet fuer DB
+@Data // automatisch Getter, Setter, toString() usw
+@NoArgsConstructor // leerer Konstruktor fuer JPA benoetigt
+@AllArgsConstructor
+
 public class Mitarbeiter {
+
+    @Id // Markiert id als Primaerschluessel
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // id wird automatisch inkrementiert
+    private String id;
+    private String anrede; // haben wir nicht im Domänenmodell, aber wäre sinnvoll?
+    private String vorname;
+    private String nachname;
+    private String telefonnummer;
+    private String email;
+    private String passwort;
+    private String abteilung;
+    private String position;
 }
