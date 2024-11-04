ae12143fa835ba9f14082b1c371479dc33077369
moniquejunique <161077262+moniquejunique@users.noreply.github.com>
2024-11-04 17:18:31 +0100

    SimpleTest Rechnung korrigiert und Benennung nach regex

--- backend/src/test/java/com/cityfeedback/backend/simpleTest.java
+++ backend/src/test/java/com/cityfeedback/backend/simpleTest.java
@@ -3,11 +3,11 @@ package com.cityfeedback.backend;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class simpleTest {
+public class SimpleTest {
 
     @Test
     void additionTest() {
-        int result = 2 + 3;
+        int result = 2 + 2;
         assertEquals(4, result, "2 + 2 sollte 4 ergeben");
     }
 }
