cd03186a6d6e3ef0182ebdefd0d5dfa00991e243
Ann-Kathrin <ann-kathrin.meyerhof@stud.th-luebeck.de>
2024-11-09 19:23:12 +0100

    bestehenden Test fuer erfolgreiche Registrieung eines Buerger ueberarbeitet und 2. Test fuer Exception hinzugefuegt

--- backend/src/test/java/com/cityfeedback/backend/BuergerTest.java
+++ backend/src/test/java/com/cityfeedback/backend/BuergerTest.java
@@ -3,6 +3,7 @@ package com.cityfeedback.backend;
 import com.cityfeedback.backend.domain.Buerger;
 import com.cityfeedback.backend.repositories.BuergerRepository;
 import com.cityfeedback.backend.services.BuergerService;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 
+import java.lang.module.ResolutionException;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -20,27 +24,66 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 public class BuergerTest {
 
+    // Testobjekte
+    Buerger testBuerger1 = new Buerger(123L, "Frau", "Maxi", "Musterfrau", "987654321", "maxi.musterfau@example.com", "StarkesPW1?");
+    Buerger testBuerger2 = new Buerger(124L, "Frau", "Julia", "Mustermann", "987654321", "maxi.musterfau@example.com", "StarkesPW1?");
+    Buerger testBuerger3 = new Buerger(125L, "Herr", "Juan", "Perez", "123456789", "juan.perez@example.com", "pinFuerte123!");
+    Buerger testBuerger4 = new Buerger(126L, "Herr", "Juan", "Perez", "123456789", "j.perez@example.com", "pinFuerte123!");
+
     @Autowired
     private BuergerService buergerService;
-
     @Autowired
     private BuergerRepository buergerRepository;
 
-    // Testobjekte
-    Buerger testBuerger1 = new Buerger("", "Herr", "Juan", "Perez", "123456789", "juan.perez@example.com", "pinFuerte123!");
-    Buerger testBuerger2 = new Buerger("2", "Frau", "Maxi", "Musterfrau", "987654321", "maxi.musterfau@example.com", "1StarkesPW?");
-
     @BeforeEach
     void setUp() {
-        // vor jedem Test
+        // vor jedem Test wird die DB geleert
+        buergerRepository.deleteAll();
+    }
+
+    @Test
+    public void registriereBuerger_sollErfolgreichSein() { // Ueberprüft, ob ein Buerger erfolgreich gespeichert werden kann.
+
+        ResponseEntity<?> response = buergerService.registriereBuerger(testBuerger3);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertTrue(buergerRepository.existsByEmail(testBuerger3.getEmail()));
 
     }
 
-   @Test
-   public void testErfolgreicheRegistrierung() {
-        ResponseEntity<?> response = buergerService.regstriereBuerger(testBuerger1);
+    @Test
+    public void registriereBuerger_sollFehlerWerfenBeiDoppelterEmail() {  // Prueft, ob bei einer bereits bestehenden E-Mail-Adresse ein Fehler zurückgegeben wird.
+        buergerRepository.save(testBuerger1);
+        ResponseEntity<?> response = buergerService.registriereBuerger(testBuerger2);
 
-       assertEquals(HttpStatus.OK, response.getStatusCode());
-       //assertTrue(buergerRepository.existsByEmail(testBuerger1.getEmail()));
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+
     }
+
+ /*  @Test
+    public void registriereBuerger_sollPasswortHashen() { // ueberprüft, ob das Passwort vor der Speicherung gehasht wird.
+        ResponseEntity<?> response = buergerService.registriereBuerger(testBuerger3);
+
+        Buerger gespeicherterBuerger = buergerRepository.findById(testBuerger3.getId()).get();
+        assertNotEquals("pinFuerte123!", gespeicherterBuerger.getPasswort());
+
+    }*/
+
+    /*@Test
+    public void loescheBuerger_sollErfolgreichSein() { // Ueberprüft, ob ein Buerger erfolgreich geloescht werden kann.
+
+        buergerRepository.save(testBuerger4);
+
+        ResponseEntity<?> response = buergerService.loescheBuerger(testBuerger4);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+       // assertFalse(buergerRepository.existsByEmail(testBuerger4.getEmail()));
+
+    }*/
+
+   /* @Test
+    public void loescheBuerger_sollExceptionWerfenWennBuergerNichtExistiert() { // Prueft, ob eine ResolutionException geworfen wird, wenn versucht wird, einen nicht existierenden Buerrger zu loeschen.
+
+        assertThrows(ResolutionException.class, () -> buergerService.loescheBuerger(testBuerger1.getId()));
+    }*/
 }
