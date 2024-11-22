af79b202f50cfb1a430aaede83bac7240fb3acbb
katja-js <katja.schneider@stud.th-luebeck.de>
2024-11-22 14:13:49 +0100

    frontend registriere Bürger und Mitarbeiter eingebunden

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import Registrieren from './pages/registrierenBuerger.js'
+import './App.css';
+import './style/style.css'
+
+function App() {
+  return (
+    <div className="App">
+        <Registrieren/>
+    </div>
+  );
+}
+
+export default App;
