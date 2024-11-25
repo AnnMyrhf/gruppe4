5b8f4d903f2fec7f33b54315ed3b25b9a9c3173d
Ann-Kathrin <ann-kathrin.meyerhof@stud.th-luebeck.de>
2024-11-25 19:39:50 +0100

    Beziehung zwischen Buerger und Beschwerde hinzugefuegt, BeschwerdeRepository implemetiert

--- backend/src/main/java/com/cityfeedback/backend/domain/Beschwerde.java
+++ backend/src/main/java/com/cityfeedback/backend/domain/Beschwerde.java
@@ -1,12 +1,7 @@
 package com.cityfeedback.backend.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.Date;
 import java.util.UUID;
@@ -15,10 +10,14 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
+
 public class Beschwerde {
     @Id
+    @Setter(AccessLevel.PUBLIC)
+    @Getter(AccessLevel.PUBLIC)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
     private Date erstellDatum;
     private String status;
     private String beschwerdeTyp;
@@ -27,35 +26,8 @@ public class Beschwerde {
     private boolean anhang;
     private String datentypAnhang;
 
-    public Beschwerde(Date ErstellDatum,
-                      String Status,
-                      String BeschwerdeTyp,
-                      String Prioritaet,
-                      String Textfeld,
-                      boolean Anhang,
-                      String DatentypAnhang)
-    {
-        this.id = UUID.randomUUID().toString();
-        this.erstellDatum = ErstellDatum;
-        this.status = Status;
-        this.beschwerdeTyp = BeschwerdeTyp;
-        this.prioritaet = Prioritaet;
-        this.textfeld = Textfeld;
-        this.anhang = Anhang;
-        this.datentypAnhang = DatentypAnhang;
-    }
+    @ManyToOne
+    @JoinColumn(name = "buerger_id")
+    private Buerger buerger;
 
-    // Getter methods for each field
-
- /*
-    public String getId() { return id; }
-    public Date getErstellDatum() { return ErstellDatum; }
-    public String getStatus() { return Status; }
-    public String getBeschwerdeTyp() { return BeschwerdeTyp; }
-    public String getPrioritaet() { return Prioritaet; }
-    public String getTextfeld() { return Textfeld; }
-    public boolean hasAnhang() { return Anhang; }
-    public String getDatentypAnhang() { return DatentypAnhang; }
-
-  */
 }
--- backend/src/main/java/com/cityfeedback/backend/repositories/BeschwerdeRepository.java
+++ backend/src/main/java/com/cityfeedback/backend/repositories/BeschwerdeRepository.java
@@ -1,4 +1,16 @@
 package com.cityfeedback.backend.repositories;
 
-public class BeschwerdeRepository {
+import com.cityfeedback.backend.domain.Beschwerde;
+import com.cityfeedback.backend.domain.Buerger;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BeschwerdeRepository extends JpaRepository<Beschwerde, Long> {
+
+    Optional<Beschwerde> findByBuerger(Buerger buerger);
+    Boolean existsByBuerger(Buerger buerger);
 }
+
