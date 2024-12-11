dcf69412803682b88c01527ea0ebc97097f4c4b8
PizzaPapst <maik.bartels2@gmail.com>
2024-12-11 21:00:16 +0100

    Frontend Dashboard

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -1,82 +1,64 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import Beschwerde from "../components/beschwerde";
-import { useNavigate } from 'react-router-dom';
+import React, {useEffect, useState, useCallback} from "react";
+import { Navigate } from 'react-router-dom';
+import { useSelector, useDispatch } from "react-redux";
+import UserService from "../services/user.service"
+import { buergerLogout } from "../actions/auth-buerger";
 
-export default function Dashboard() {
+const Profile = () => {
+
     const [beschwerden, setBeschwerden] = useState([]);
+    const dispatch = useDispatch();
+    const { user: currentUser } = useSelector((state) => state.auth);
 
-    const navigate = useNavigate();
+    useEffect(() => {
+        UserService.getBuergerDashBoard().then(
+            (response) => {
+                setBeschwerden(response.data);
+            },
+            (error) => {
+                const _content =
+                    (error.response &&
+                        error.response.data &&
+                        error.response.data.message) ||
+                    error.message ||
+                    error.toString();
+                setBeschwerden(_content);
+                console.log(_content)
+            }
+        );
+    }, []);
 
-    const handleClick = (e) => {
+    const logOut = useCallback(() => {
+        dispatch(buergerLogout());
+    }, [dispatch]);
 
-        if (true) {
-            navigate('/neuebeschwerde', { replace: true });
-        } 
-    } 
-
-    // useEffect(() => {
-    //         fetch('http://localhost:8081/beschwerde/findByBuerger', {
-    //             method: 'POST',
-    //             headers: {
-    //               'Content-Type': 'application/json', // Header für JSON-Inhalt
-    //             },
-    //             body: JSON.stringify(1), // Verwende das aktualisierte Mitarbeiter-Objekt
-    //           })
-    //             .then((response) => response.text()) // Antwort als Text umwandeln
-    //             .then((data) => {
-    //                 setBeschwerden(data);
-    //                 console.log('Server Response:', data);
-    //             })
-    // }, []);
-
-    // Styles
-    const mainStyle = {
-        height: "100%",
-        margin: 0,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "start",
-        alignItems: "center",
-        backgroundColor: "#F2F6FE",
-        gap: "32px",
-        padding: "64px",
-    };
-
-    const cardStyle = {
-        display: "flex",
-        flexDirection: "column",
-        width: "100%",
-        maxWidth: "1200px",
-        border: "1px solid #ddd",
-        padding: "32px",
-        borderRadius: "8px",
-        backgroundColor: "#ffffff",
-        boxShadow: "0 2px 10px rgba(0, 0, 0, 0.1)",
-        gap: "32px",
-        textDecoration: "none",
-        color: "inherit"
-
-    };
+    if (!currentUser) {
+        return <Navigate to="/buerger-anmelden" />;
+    }
 
     return (
-        <main style={mainStyle}>
-            {beschwerden.length === 0 ? (
-                <p>Keine Beschwerden vorhanden</p>
-            ) : (
-                beschwerden.map((beschwerde) => (
-                    <Link
-                        key={beschwerde.id}
-                        to={`/mitarbeiter/dashboard/${beschwerde.id}`} // Navigiere zur Detail-Seite mit ID
-                        state={{ beschwerden }} // Übergebe die Beschwerden als State
-                        style={cardStyle}
-                    >
-                        <Beschwerde beschwerde={beschwerde} />
-                    </Link>
-                ))
-            )}
+        <div className="container">
 
-<button type="button" onClick={handleClick}>Neue Beschwerde anlegen</button>
-        </main>
+                <h3>
+                    <strong>{currentUser.vorname} {currentUser.nachname}</strong> Profile
+                </h3>
+
+            <p>
+                <strong>Token:</strong> {currentUser.accessToken.substring(0, 20)} ...{" "}
+                {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
+            </p>
+            <p>
+                <strong>Telefonnummer:</strong> {currentUser.telefonnummer}
+            </p>
+            <p>
+                <strong>Email:</strong> {currentUser.email}
+            </p>
+            <strong>Authorities:</strong> {currentUser.role[0].authority} <br/>
+           <a href="/buerger-anmelden" className="nav-link" onClick={logOut}>
+                LogOut
+            </a>
+        </div>
     );
-}
+};
+
+export default Profile;
