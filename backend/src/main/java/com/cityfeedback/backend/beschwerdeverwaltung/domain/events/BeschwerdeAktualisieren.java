70eb82e3558af72ac689d72f7b615fb57b8d7ead
Ann-Kathrin <ann-kathrin.meyerhof@stud.th-luebeck.de>
2024-12-22 13:25:30 +0100

    Erste Implementierung von DomainEvents für Beschwerde-Entität (noch unvollstaendig)

---
+++ backend/src/main/java/com/cityfeedback/backend/beschwerdeverwaltung/domain/events/BeschwerdeAktualisieren.java
@@ -0,0 +1,39 @@
+package com.cityfeedback.backend.beschwerdeverwaltung.domain.events;
+
+import com.cityfeedback.backend.beschwerdeverwaltung.domain.model.Beschwerde;
+import com.cityfeedback.backend.beschwerdeverwaltung.domain.valueobjects.Prioritaet;
+import com.cityfeedback.backend.beschwerdeverwaltung.domain.valueobjects.Status;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BeschwerdeAktualisieren {
+
+
+        @Id // Markiert id als Primaerschluessel
+        @GeneratedValue(strategy = GenerationType.IDENTITY) // id wird automatisch inkrementiert
+        private Long id;
+        private Timestamp timestamp;
+        private String titel;
+        private Status status;
+        private Prioritaet prioritaet;
+
+        public BeschwerdeAktualisieren(Beschwerde beschwerde) {
+            this.id = beschwerde.getId();
+            this.timestamp = new Timestamp(System.currentTimeMillis());
+            this.titel = beschwerde.getTitel();
+            this.status = beschwerde.getStatus();
+            this.prioritaet = beschwerde.getPrioritaet();
+        }
+    }
+
